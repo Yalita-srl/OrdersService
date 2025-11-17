c3b4f0b76295a57828e1a473ad89f082d59c1f00
seVeloper <sergioveloper@gmail.com>
2025-11-17 19:43:48 -0400

    contenedores funcinales

--- src/modules/orders/application/services/order.service.ts
+++ src/modules/orders/application/services/order.service.ts
@@ -46,7 +46,7 @@ export class OrderService {
 
   async publishToRabbitMQ(queue: string, data: any) {
     try {
-      const connection = await amqp.connect('amqp://localhost:5672');
+      const connection = await amqp.connect('amqp://rabbitmq:5672');
       const channel = await connection.createChannel();
       
       await channel.assertQueue(queue, { durable: true });
